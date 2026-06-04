c7d4f9dbc26fd5384ad43923b06218444b950369
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2026-06-04 13:00:08 +1200

    have a character who is red, this wll be player 1

--- 2D Shooter.py
+++ 2D Shooter.py
@@ -8,11 +8,20 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("2D Shooter")
 
-BACKGROUND_COLOUR = (255, 255, 255)
-
 clock = pygame.time.Clock()
 
+BACKGROUND_COLOUR = (255, 255, 255)
+
+player_colour = (225, 0, 0)
+player_width = 25
+player_height = 25
+
+player_x = 300
+player_y = 300
+player_speed = 5
+
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,6 +29,8 @@ while running:
 
     screen.fill(BACKGROUND_COLOUR)
  
+    pygame.draw.rect(screen, player_colour, (player_x, player_y, player_width, player_height))
+
     pygame.display.flip()
 
     clock.tick(60)
