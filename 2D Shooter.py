13a9a87e7522a1b75f7187fc9be969bcbd81fdfa
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2026-06-09 11:25:46 +1200

    chaged the player into a class, this will be very important as this basically furutre proofs the code in a sense that it needs to interact with other variables

--- 2D Shooter.py
+++ 2D Shooter.py
@@ -3,24 +3,69 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 800
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("2D Shooter")
 
 clock = pygame.time.Clock()
 
 BACKGROUND_COLOUR = (255, 255, 255)
+black = (0,0,0)
 
-player_colour = (225, 0, 0)
-player_width = 25
-player_height = 25
 
-player_x = 300
-player_y = 300
-player_speed = 3
+class Players:
+    def __init__(self):
+        self.x = 0
+        self.y = 800
+
+        self.width = 25
+        self.height = 25
+        self.colour = (225, 0, 0)
+
+        self.speed = 4
+
+        self.colour = (255, 0, 0)
+
+    def movement(self, keys):
+        if keys[pygame.K_a]:
+            self.x -= self.speed
+        
+        if keys[pygame.K_d]:
+            self.x += self.speed
+        
+        if keys[pygame.K_w]:
+            self.y -= self.speed
+        
+        if keys[pygame.K_s]:
+            self.y += self.speed
+
+    def boundrys(self):
+        if self.x < 0:
+            self.x = 0
+
+        if self.y < 0:
+            self.y = 0
+
+        if self.x > SCREEN_WIDTH - self.width:
+            self.x = SCREEN_WIDTH - self.width
+
+        if self.y > SCREEN_HEIGHT - self.height:
+            self.y = SCREEN_HEIGHT - self.height
+
+    def draw(self, screen):
+        pygame.draw.rect(
+            screen,
+            self.colour,
+            (self.x, self.y, self.width, self.height)
+        )
+
+
+player = Players()
 
 running = True
+
 
 while running:
     for event in pygame.event.get():
@@ -29,32 +74,17 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_a]:
-        player_x -= player_speed
-    if keys[pygame.K_d]:
-        player_x += player_speed
-    if keys[pygame.K_w]:
-        player_y -= player_speed
-    if keys[pygame.K_s]:
-        player_y += player_speed
-
-    if player_x < 0:
-        player_x = 0
-    if player_y < 0:
-        player_y = 0
-    if player_x > SCREEN_WIDTH - player_width:
-        player_x = SCREEN_WIDTH - player_width
-    if player_y > SCREEN_HEIGHT - player_height:
-        player_y = SCREEN_HEIGHT - player_height
-
+    player.movement(keys)
+    player.boundrys()
 
     screen.fill(BACKGROUND_COLOUR)
- 
-    pygame.draw.rect(screen, player_colour, (player_x, player_y, player_width, player_height))
+
+    player.draw(screen)
 
     pygame.display.flip()
 
     clock.tick(60)
+
 
 pygame.quit()
 sys.exit()
